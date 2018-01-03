82a23d27fd9148726d8083e3afe179cbc71c6e3c
chenyulin <capslock.ch@outlook.com>
2018-01-03 18:36:54 +0800

    实现懒加载

--- js/w.js
+++ js/w.js
@@ -1,6 +1,6 @@
-var imageList = function () {
+var imageList = function (n = 8) {
     let result = []
-    for (let i = 1; i <= 38; i++) {
+    for (let i = 1; i <= n; i++) {
         let img = {
             href: ``,
             message: `存放一些信息`,
@@ -16,19 +16,32 @@ var addImage = function (imageParent, img) {
     let message = img.message
     let htmlText = `
         <a class="article" href="${href}">
-            <img src="${url}" />
+            <img data-src="${url}" class="lazyLoad" />
             <h2>${message}</h2>
         </a>
     `
     imageParent.insertAdjacentHTML('beforeend', htmlText)
 }
+/* 添加占位图
+ * 
+ */
+var addBitmap = function () {
+    let lazyClass = ".lazyLoad"
+    let _src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC'
+    let images = document.querySelectorAll(lazyClass)
+    console.log(images.length)
+    images.forEach(img => {
+        img.src = _src
+    })
+}
 
-var loadImage = function () {
-    let imgList = imageList()
+var loadImage = function (n = 8) {
+    let imgList = imageList(n)
     let imageParent = document.querySelector('#id-images-all')
     imgList.forEach(img => {
         addImage(imageParent, img)
     })
+    addBitmap()
 }
 
 var jaLisWallInit = function () {
@@ -39,9 +52,13 @@ var jaLisWallInit = function () {
     })
 }
 
-const _main = function () {
-    loadImage()
+var libraryInit = function () {
     jaLisWallInit()
+}
+
+const _main = function () {
+    loadImage(74)
+    libraryInit()
 }
 
 _main()
