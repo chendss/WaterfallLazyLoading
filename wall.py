0d2321d607f834586332babef9b63dee73c74ba8
chenyulin <capslock.ch@outlook.com>
2018-01-09 19:24:25 +0800

    实现动态瀑布流

--- wall.py
+++ wall.py
@@ -1,36 +1,9 @@
 from flask import Flask, send_file, request  # , redirect, abort
 from gevent.wsgi import WSGIServer
 import json
-import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
-
-def base_body(url):
-    """
-    获得一个网页的返回值
-    :param url:空
-    :return: body
-    """
-
-    s = requests.session()
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/60.0.3112.90 Safari/537.36',
-        'X-Requested-With': 'XMLHttpRequest',
-    }
-
-    r = s.get(url, headers=headers)
-    text = r.text
-
-    return text
-
-
-def h_images(src):
-    body = json.loads(base_body(src))
-    pins = ['http://img.hb.aicdn.com/' + p['file']['key'] + '_fw236' for p in body['pins']]
-    return pins
 
 
 @app.route('/', methods=['GET'])
@@ -38,25 +11,25 @@ def night():
     return send_file('html/w.html')
 
 
-def get_src(args):
-    src = 'http://huaban.com/search/?q=变形金刚'
-    if len(args) != 0:
-        page = args['page']
-        src = 'http://huaban.com/search/?q=%E5%8F%98%E5%BD%A2%E9%87%91%E5%88%9A&jc0ci1kw&page={}&per_page=20&wfl=1'
-        src = src.format(page)
-    return src
+def url_array():
+    with open('resource/url.txt') as f:
+        str_ = f.read()
+    url_list = json.loads(str_)
+    return url_list
+
+
+def page_url(page):
+    n = 20
+    url_list = url_array()
+    url_list_split = [url_list[i:i + n] for i in range(0, len(url_list), n)]
+    return url_list_split[page]
 
 
 @app.route('/images', methods=['GET'])
 def images():
     args = request.args
-    src = get_src(args)
-    img_list = h_images(src)
-    img_dict = {
-        'list': img_list,
-        'length': len(img_list),
-    }
-    return json.dumps(img_dict)
+    page = args['page']
+    return page_url(page)
 
 
 @app.route('/<path:fn>', )
